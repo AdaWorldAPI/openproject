371497b3c06c78eed7b1fd51953b3b3e8ccd0179
AdaWorldAPI <jan@exo.red>
2026-01-26 16:25:00 +0100

    Add MS Graph Mail module: openproject-msgraph_mail.rb

---
+++ modules/msgraph_mail/lib/openproject-msgraph_mail.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject MS Graph Mail Module
+# Copyright (C) 2025 Jan Hübener / DATAGROUP SE
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#++
+
+require "open_project/msgraph_mail"
