7cd4193a697a3e33887d7f9f85a0c91cf6dc6ebe
AdaWorldAPI <jan@exo.red>
2026-01-27 17:23:36 +0100

    feat: add RESET_ADMIN_PASSWORD env var to reset admin on boot

--- docker/prod/entrypoint-slim.sh
+++ docker/prod/entrypoint-slim.sh
@@ -14,4 +14,21 @@ if [ -n "$PGVERSION" ]; then
 	export PATH="$PGBIN:$PATH"
 fi
 
+# ONE-TIME: Reset admin password if RESET_ADMIN_PASSWORD is set
+if [ -n "$RESET_ADMIN_PASSWORD" ]; then
+	echo "==> Resetting admin password..."
+	bundle exec rails runner "
+		admin = User.find_by(login: 'admin')
+		if admin
+			admin.password = ENV['RESET_ADMIN_PASSWORD']
+			admin.password_confirmation = ENV['RESET_ADMIN_PASSWORD']
+			admin.failed_login_count = 0
+			admin.save!(validate: false)
+			puts 'Admin password reset and lockout cleared!'
+		else
+			puts 'Admin user not found!'
+		end
+	" || echo "Password reset failed, continuing..."
+fi
+
 exec "$@"
